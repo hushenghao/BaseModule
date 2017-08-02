29a116b869942be0b00a89dd914a833d56f9942a
胡晟昊 <863116814@qq.com>
2017-08-02 14:24:09 +0800

    fix

--- baseModule/src/main/java/com/dede/basemodule/base/activity/RxBaseActivity.java
+++ baseModule/src/main/java/com/dede/basemodule/base/activity/RxBaseActivity.java
@@ -1,6 +1,8 @@
 package com.dede.basemodule.base.activity;
 
 import android.os.Bundle;
+import android.support.annotation.CallSuper;
+import android.support.annotation.CheckResult;
 import android.support.annotation.NonNull;
 
 import com.trello.rxlifecycle2.LifecycleProvider;
@@ -23,52 +25,61 @@ public abstract class RxBaseActivity extends BaseActivity implements LifecyclePr
 
     @NonNull
     @Override
+    @CheckResult
     public <T> LifecycleTransformer<T> bindUntilEvent(@NonNull ActivityEvent event) {
         return RxLifecycle.bindUntilEvent(lifecycleSubject, event);
     }
 
     @NonNull
     @Override
+    @CheckResult
     public <T> LifecycleTransformer<T> bindToLifecycle() {
         return RxLifecycleAndroid.bindActivity(lifecycleSubject);
     }
 
     @NonNull
+    @CheckResult
     public Observable<ActivityEvent> lifecycle() {
         return lifecycleSubject.hide();
     }
 
     @Override
+    @CallSuper
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         lifecycleSubject.onNext(ActivityEvent.CREATE);
     }
 
     @Override
+    @CallSuper
     protected void onStart() {
         super.onStart();
         lifecycleSubject.onNext(ActivityEvent.START);
     }
 
     @Override
+    @CallSuper
     protected void onResume() {
         super.onResume();
         lifecycleSubject.onNext(ActivityEvent.RESUME);
     }
 
     @Override
+    @CallSuper
     protected void onPause() {
         super.onPause();
         lifecycleSubject.onNext(ActivityEvent.PAUSE);
     }
 
     @Override
+    @CallSuper
     protected void onStop() {
         super.onStop();
         lifecycleSubject.onNext(ActivityEvent.STOP);
     }
 
     @Override
+    @CallSuper
     protected void onDestroy() {
         super.onDestroy();
         lifecycleSubject.onNext(ActivityEvent.DESTROY);
--- baseModule/src/main/java/com/dede/basemodule/base/fragment/RxBaseFragment.java
+++ baseModule/src/main/java/com/dede/basemodule/base/fragment/RxBaseFragment.java
@@ -2,6 +2,8 @@ package com.dede.basemodule.base.fragment;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.CallSuper;
+import android.support.annotation.CheckResult;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.View;
@@ -26,76 +28,89 @@ public abstract class RxBaseFragment extends BaseFragment implements LifecyclePr
 
     @NonNull
     @Override
+    @CheckResult
     public <T> LifecycleTransformer<T> bindUntilEvent(@NonNull FragmentEvent event) {
         return RxLifecycle.bindUntilEvent(lifecycleSubject, event);
     }
 
     @NonNull
     @Override
+    @CheckResult
     public <T> LifecycleTransformer<T> bindToLifecycle() {
         return RxLifecycleAndroid.bindFragment(lifecycleSubject);
     }
 
     @NonNull
+    @CheckResult
     public Observable<FragmentEvent> lifecycle() {
         return lifecycleSubject.hide();
     }
 
     @Override
+    @CallSuper
     public void onAttach(Context context) {
         super.onAttach(context);
         lifecycleSubject.onNext(FragmentEvent.ATTACH);
     }
 
     @Override
+    @CallSuper
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        lifecycleSubject.onNext(FragmentEvent.ATTACH);
+        lifecycleSubject.onNext(FragmentEvent.CREATE);
     }
 
     @Override
+    @CallSuper
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         lifecycleSubject.onNext(FragmentEvent.CREATE_VIEW);
     }
 
     @Override
+    @CallSuper
     public void onStart() {
         super.onStart();
         lifecycleSubject.onNext(FragmentEvent.START);
     }
 
     @Override
+    @CallSuper
     public void onResume() {
         super.onResume();
         lifecycleSubject.onNext(FragmentEvent.RESUME);
     }
 
     @Override
+    @CallSuper
     public void onPause() {
         super.onPause();
         lifecycleSubject.onNext(FragmentEvent.PAUSE);
     }
 
     @Override
+    @CallSuper
     public void onStop() {
         super.onStop();
         lifecycleSubject.onNext(FragmentEvent.STOP);
     }
 
     @Override
+    @CallSuper
     public void onDestroyView() {
         super.onDestroyView();
         lifecycleSubject.onNext(FragmentEvent.DESTROY_VIEW);
     }
 
     @Override
+    @CallSuper
     public void onDestroy() {
         super.onDestroy();
         lifecycleSubject.onNext(FragmentEvent.DESTROY);
     }
 
     @Override
+    @CallSuper
     public void onDetach() {
         super.onDetach();
         lifecycleSubject.onNext(FragmentEvent.DETACH);
